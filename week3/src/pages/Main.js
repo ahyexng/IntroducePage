471d39f2f8d9b4db082df61e9f358c35ac007ff6
ahyexng <kay561834@gmail.com>
2023-04-26 15:40:44 +0900

    우리집강아지추가

--- week3/src/pages/Main.js
+++ week3/src/pages/Main.js
@@ -1,12 +1,14 @@
+import React, { useEffect, useState } from "react";
 import "../Main.css";
 import InfoList from "./InfoList";
 import IntroduceList from "./IntroduceList";
 import data from "./Data.json";
 import Side from "./Side";
 import Sns from "./Sns";
+import Dog from "./Dog";
 
 const Main = () => {
-
+    const [click, setClick] = useState(false);
 return (
     <>
     <Side data = {data} />
@@ -15,7 +17,10 @@ return (
     <IntroduceList data = {data}  />
     </div>
     <Sns data = {data} />
-    
+    <div className="dogImg">
+    <button onClick={()=>{setClick(!click)}} className="btn">우리집 강아지</button>
+    {click && <Dog/>}
+    </div>
     </>
     )
 };
